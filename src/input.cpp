ab4923cb144eeea6a193f0c1aacff25ced094286
Nico <nb287@uni-rostock.de>
2022-10-02 01:10:58 +0200

    added vertical movement

--- src/input.cpp
+++ src/input.cpp
@@ -6,7 +6,13 @@ Input::Input(GLFWwindow& window, Camera& camera) : window(window), camera(camera
 }
 
 void Input::moveCameraCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
-        pcamera->moveCamera(glm::vec3(0.0, 1.0, 0.0), 1.0f);
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        pcamera->moveCamera(glm::vec3(0.0, 1.0, 0.0), 0.1f);
     }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+        pcamera->moveCamera(glm::vec3(0.0, -1.0, 0.0), 0.1f);
+    }
+    // if (key == GLFW_KEY_A && action == GLFW_PRESS) {
+    //     pcamera->moveCamera(glm::vec3(0.0, -1.0, 0.0), 1.0f);
+    // }
 }
