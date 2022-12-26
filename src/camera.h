8bea2d4f8a2139adb8d40da908ad2e083adc1880
ksknico <nifrabe@outlook.de>
2022-12-26 23:55:06 +0100

    changed include statement for glad.h

--- src/camera.h
+++ src/camera.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <glad/glad.h>
+#include "glad.h"
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <glad/glad.h>
+#include "glad.h"
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
