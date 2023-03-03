e8686e6006ffbb99d253ececfff0de87eb29b645
ksknico <nifrabe@outlook.de>
2023-03-03 03:22:37 +0100

    blocks is now a multi-dimensional vector

--- src/world.cpp
+++ src/world.cpp
@@ -1,28 +1,71 @@
 #include "world.h"
 
 
-World::World(int sizeX, int sizeY, Player &player) : player(player) {
+World::World(int sizeX, int sizeY, int sizeZ, Player &player) : sizeX(sizeX), sizeY(sizeY), sizeZ(sizeZ), player(player) {
     std::random_device rd; // obtain a random number from hardware
     std::mt19937 gen(rd()); // seed the generator
-    std::uniform_int_distribution<> distr(0, 10);
-    this->blocks = std::map<std::tuple<int, int, int>, Block>();
+    std::uniform_int_distribution<> distr(0, 9);
 
-    for (int i = 0; i <= sizeX; i++){
-        for (int j = 0; j <= sizeY; j++) {
-            int randomNumber = distr(gen);
-            this->blocks.insert(
-                {
-                    std::make_tuple(i, randomNumber, j),
-                    Block(
-                        glm::vec3(i, randomNumber, j)
-                    )
+    this->blocks = std::vector<std::vector<std::vector<Block>>>();
+
+    for (int i = 0; i < sizeX; i++){
+        std::vector<std::vector<Block>> xVector = std::vector<std::vector<Block>>();
+        for (int j = 0; j < sizeY; j++) {
+            std::vector<Block> yVector = std::vector<Block>();
+            for (int k = 0; k < sizeZ; k++) {
+                int randomNumber = distr(gen);
+                if (randomNumber >= 10) {
+                    yVector.push_back(Block(glm::vec3(i, j, k), Block::Type::AIR));
+                } else {
+                    yVector.push_back(Block(glm::vec3(i, j, k), Block::Type::SOLID));
                 }
-            );
+            }
+            xVector.push_back(yVector);
+        }
+        this->blocks.push_back(xVector);
+    }
+
+    // steps over all blocks and determines visibility
+    for (int i = 0; i < sizeX; i++){
+        for (int j = 0; j < sizeY; j++) {
+            for (int k = 0; k < sizeZ; k++) {
+                this->blocks[i][j][k].hidden = isHidden(glm::vec3(i, j, k));
+            }
         }
     }
+
 }
 
-void World::populateWorld(int sizeX, int sizeY, int height) {
+
+
+bool World::isHidden(const glm::vec3 &position) const {
+    if (position[0] == sizeX-1 || position[0] == 0 || position[1] == sizeY-1 || position[1] == 0 || position[2] == sizeZ-1 || position[2] == 0) {
+        return false;
+    } else if (
+        blocks[position[0]+1][position[1]][position[2]].isSolid()
+        &&
+        blocks[position[0]-1][position[1]][position[2]].isSolid()
+        &&
+        blocks[position[0]][position[1]+1][position[2]].isSolid()
+        &&
+        blocks[position[0]][position[1]-1][position[2]].isSolid()
+        &&
+        blocks[position[0]][position[1]][position[2]+1].isSolid()
+        &&
+        blocks[position[0]][position[1]][position[2]-1].isSolid()
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+bool World::isHidden(const Block &block) const {
+    return this->isHidden(block.position);
+}
+
+// void World::populateWorld(int sizeX, int sizeY, int sizeZ, int height) {
     // this->blocks = std::vector<Block>();
     // std::random_device rd;
     // std::mt19937 gen(rd()); // seed the generator
@@ -38,11 +81,11 @@ void World::populateWorld(int sizeX, int sizeY, int height) {
     //         }
     //     }
     // }
-}
+// }
 
 void World::calculatePhysics() {
     // If next location is in bounding box of cube, set player's speed to 0
-    glm::vec3 nextLocation = player.position + player.speed;
+/*     glm::vec3 nextLocation = player.position + player.speed;
     for (auto pair : blocks) {
         if (nextLocation[0] < pair.second.position[0]+1 && 
         nextLocation[0] > pair.second.position[0] &&
@@ -55,14 +98,14 @@ void World::calculatePhysics() {
     }
     player.movePlayer(nextLocation);
     // this is the gravity added onto the speed
-    player.speed += glm::vec3(0.0f, -0.01f, 0.0f);
+    player.speed += glm::vec3(0.0f, -0.01f, 0.0f); */
 }
 
 
 
 Block& closestBlockInLOS(glm::vec3 &position, glm::vec3 &direction, double maxRange) {
     // bound check
-    double x1 = int(position[0]-maxRange);
+/*     double x1 = int(position[0]-maxRange);
     double x2 = int(position[0]+maxRange);
 
     double y1 = int(position[1]-maxRange);
@@ -77,5 +120,5 @@ Block& closestBlockInLOS(glm::vec3 &position, glm::vec3 &direction, double maxRa
                 
             }
         }
-    }
+    } */
 }
--- src/world.h
+++ src/world.h
@@ -11,15 +11,28 @@ const float gravitation = 1.0f;
 
 class World {
     public:
-        World(int sizeX, int sizeY, Player &player);
-        std::map<std::tuple<int, int, int>, Block> blocks;
+        World(int sizeX, int sizeY, int sizeZ, Player &player);
+        std::vector<std::vector<std::vector<Block>>> blocks;
+        int sizeX;
+        int sizeY;
+        int sizeZ;
         Player &player;
         void calculatePhysics();
-        void populateWorld(int sizeX, int sizeY, int height);
+        // void populateWorld(int sizeX, int sizeY, int sizeZ, int height);
 
-        /// @brief Calculates the closest block when filling out when drawing a line starting from position in the specified direction. 
-        /// @param position The position from where to draw the line
-        /// @param direction The direction in which the line is to be drawn
+        /// @brief For the position of a block returns true when that block is only surrounded by blocks that are solid
+        /// @param position Position of the that block
+        /// @return True if block is hidden, false otherwise
+        bool isHidden(const glm::vec3 &position) const;
+
+        /// @brief For a block returns true when that block is only surrounded by blocks that are solid
+        /// @param block Block to be checked 
+        /// @return True if block is hidden, false otherwise
+        bool isHidden(const Block &block) const;
+
+        /// @brief Calculates the closest block when filling out when drawing a line starting from position in the specified direction
+        /// @param position Position from where to draw the line
+        /// @param direction Direction in which the line is to be drawn
         /// @param maxRange Stops if no block is found whose distance is closer or equal to maxRange
         /// @return A reference to the closest block
         Block& closestBlockInLOS(glm::vec3 &position, glm::vec3 &direction, double maxRange);        
